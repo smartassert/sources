b0e9f74eeb730b6414bdbb564c55cc8f29b1e19b
Jon Cram <webignition@gmail.com>
2021-12-13 16:02:42 +0000

    Create SourceFactory

---
+++ src/Services/SourceFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Entity\Source;
+use App\Repository\SourceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Uid\Ulid;
+
+class SourceFactory
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private SourceRepository $repository,
+    ) {
+    }
+
+    public function create(string $userId, string $hostUrl, string $path, ?string $accessToken): Source
+    {
+        $source = $this->repository->findOneBy([
+            'userId' => $userId,
+            'hostUrl' => $hostUrl,
+            'path' => $path,
+        ]);
+
+        if ($source instanceof Source) {
+            return $source;
+        }
+
+        $source = new Source((string) new Ulid(), $userId, $hostUrl, $path, $accessToken);
+
+        $this->entityManager->persist($source);
+        $this->entityManager->flush();
+
+        return $source;
+    }
+}
