8740e7d377c78706ee6c3b60e7915074bca01f3c
Jon Cram <webignition@gmail.com>
2021-12-22 09:47:38 +0000

    Inject source object into controller actions

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 
 use App\Entity\GitSource;
 use App\Entity\SourceInterface;
-use App\Repository\SourceRepository as Repository;
 use App\Request\GitSourceRequest;
 use App\Services\Source\Factory;
 use App\Services\Source\Mutator;
@@ -23,7 +22,6 @@ class SourceController
 
     public function __construct(
         private Factory $factory,
-        private Repository $repository,
         private Store $store,
         private Mutator $mutator
     ) {
@@ -36,17 +34,17 @@ class SourceController
     }
 
     #[Route(self::ROUTE_SOURCE . '{sourceId<[A-Z90-9]{26}>}', name: 'get', methods: ['GET'])]
-    public function get(string $sourceId, UserInterface $user): JsonResponse
+    public function get(?SourceInterface $source, UserInterface $user): JsonResponse
     {
-        return $this->doAction($sourceId, $user, function (SourceInterface $source): JsonResponse {
+        return $this->doAction($source, $user, function (SourceInterface $source): JsonResponse {
             return new JsonResponse($source);
         });
     }
 
     #[Route(self::ROUTE_SOURCE . '{sourceId<[A-Z90-9]{26}>}', name: 'update', methods: ['PUT'])]
-    public function update(string $sourceId, Request $request, UserInterface $user): JsonResponse
+    public function update(?SourceInterface $source, Request $request, UserInterface $user): JsonResponse
     {
-        return $this->doAction($sourceId, $user, function (SourceInterface $source) use ($request): JsonResponse {
+        return $this->doAction($source, $user, function (SourceInterface $source) use ($request): JsonResponse {
             if (!$source instanceof GitSource) {
                 return new JsonResponse(null, 404);
             }
@@ -58,9 +56,9 @@ class SourceController
     }
 
     #[Route(self::ROUTE_SOURCE . '{sourceId<[A-Z90-9]{26}>}', name: 'delete', methods: ['DELETE'])]
-    public function delete(string $sourceId, UserInterface $user): JsonResponse
+    public function delete(?SourceInterface $source, UserInterface $user): JsonResponse
     {
-        return $this->doAction($sourceId, $user, function (SourceInterface $source): JsonResponse {
+        return $this->doAction($source, $user, function (SourceInterface $source): JsonResponse {
             $this->store->remove($source);
 
             return new JsonResponse(null, 200);
@@ -70,9 +68,8 @@ class SourceController
     /**
      * @param callable(SourceInterface): JsonResponse $action
      */
-    private function doAction(string $sourceId, UserInterface $user, callable $action): JsonResponse
+    private function doAction(?SourceInterface $source, UserInterface $user, callable $action): JsonResponse
     {
-        $source = $this->repository->find($sourceId);
         if (null === $source) {
             return new JsonResponse(null, 404);
         }
