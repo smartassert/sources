a4d818fdd556719f250c3f819eb91927b219857e
Jon Cram <webignition@gmail.com>
2022-01-04 17:59:40 +0000

    Use finder in factory

--- src/Services/Source/Factory.php
+++ src/Services/Source/Factory.php
@@ -7,9 +7,6 @@ namespace App\Services\Source;
 use App\Entity\FileSource;
 use App\Entity\GitSource;
 use App\Entity\RunSource;
-use App\Repository\FileSourceRepository;
-use App\Repository\GitSourceRepository;
-use App\Repository\RunSourceRepository;
 use App\Request\FileSourceRequest;
 use App\Request\GitSourceRequest;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -19,9 +16,7 @@ class Factory
 {
     public function __construct(
         private Store $store,
-        private GitSourceRepository $gitSourceRepository,
-        private FileSourceRepository $fileSourceRepository,
-        private RunSourceRepository $runSourceRepository,
+        private Finder $finder,
     ) {
     }
 
@@ -32,36 +27,33 @@ class Factory
     {
         ksort($parameters);
 
-        $source = $this->runSourceRepository->findOneBy([
-            'parent' => $parent,
-            'parameters' => $parameters,
-        ]);
-
-        if ($source instanceof RunSource) {
-            return $source;
-        }
-
         return new RunSource($this->generateId(), $parent, $parameters);
     }
 
     public function createGitSourceFromRequest(UserInterface $user, GitSourceRequest $request): GitSource
     {
-        $source = $this->createGitSource(
+        $source = new GitSource(
+            $this->generateId(),
             $user->getUserIdentifier(),
             $request->getHostUrl(),
             $request->getPath(),
             $request->getAccessToken()
         );
 
-        $this->store->add($source);
+        if (null === $this->finder->find($source)) {
+            $this->store->add($source);
+        }
 
         return $source;
     }
 
     public function createFileSourceFromRequest(UserInterface $user, FileSourceRequest $request): FileSource
     {
-        $source = $this->createFileSource($user->getUserIdentifier(), $request->getLabel());
-        $this->store->add($source);
+        $source = new FileSource($this->generateId(), $user->getUserIdentifier(), $request->getLabel());
+
+        if (null === $this->finder->find($source)) {
+            $this->store->add($source);
+        }
 
         return $source;
     }
@@ -69,34 +61,5 @@ class Factory
     private function generateId(): string
     {
         return (string) new Ulid();
-    }
-
-    private function createGitSource(string $userId, string $hostUrl, string $path, ?string $accessToken): GitSource
-    {
-        $source = $this->gitSourceRepository->findOneBy([
-            'userId' => $userId,
-            'hostUrl' => $hostUrl,
-            'path' => $path,
-        ]);
-
-        if ($source instanceof GitSource) {
-            return $source;
-        }
-
-        return new GitSource($this->generateId(), $userId, $hostUrl, $path, $accessToken);
-    }
-
-    private function createFileSource(string $userId, string $label): FileSource
-    {
-        $source = $this->fileSourceRepository->findOneBy([
-            'userId' => $userId,
-            'label' => $label,
-        ]);
-
-        if ($source instanceof FileSource) {
-            return $source;
-        }
-
-        return new FileSource($this->generateId(), $userId, $label);
     }
 }
