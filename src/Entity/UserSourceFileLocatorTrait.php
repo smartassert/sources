090c77b719a507b8f4ed26d4a9e81da79f866ced
Jon Cram <webignition@gmail.com>
2022-01-04 11:23:10 +0000

    Add FileLocatorInterface::__toString() (#127)

--- src/Entity/UserSourceFileLocatorTrait.php
+++ src/Entity/UserSourceFileLocatorTrait.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Model\StringableFileLocatorTrait;
+
 trait UserSourceFileLocatorTrait
 {
+    use StringableFileLocatorTrait;
+
     public function getPath(): string
     {
         return sprintf(
