3ca42db3aa53957b09d22798a6a720c783426686
Jon Cram <webignition@gmail.com>
2022-01-08 15:18:05 +0000

    Create CommandDefinition model (#170)

--- src/Services/Process/Executor.php
+++ src/Services/Process/Executor.php
@@ -5,26 +5,23 @@ declare(strict_types=1);
 namespace App\Services\Process;
 
 use App\Exception\ProcessExecutorException;
+use App\Model\CommandDefinition;
 use App\Model\ProcessOutput;
 use Symfony\Component\Process\Exception\ExceptionInterface;
 
 class Executor
 {
     public function __construct(
-        private CommandBuilder $commandBuilder,
         private Factory $factory,
     ) {
     }
 
     /**
-     * @param array<string, string> $parameters
-     *
      * @throws ProcessExecutorException
      */
-    public function execute(string $command, array $parameters = [], ?string $cwd = null): ProcessOutput
+    public function execute(CommandDefinition $commandDefinition, ?string $cwd = null): ProcessOutput
     {
-        $builtCommand = $this->commandBuilder->build($command, $parameters);
-        $process = $this->factory->create($builtCommand, $cwd);
+        $process = $this->factory->create((string) $commandDefinition, $cwd);
 
         try {
             $exitCode = $process->run();
