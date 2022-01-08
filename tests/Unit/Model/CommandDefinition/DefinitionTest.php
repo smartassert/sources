34f9ab150eb13c2dc66cf62c60adaca072eaefc8
Jon Cram <webignition@gmail.com>
2022-01-08 18:22:34 +0000

    Define command definition options and arguments (#177)

---
+++ tests/Unit/Model/CommandDefinition/DefinitionTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\CommandDefinition;
+
+use App\Model\CommandDefinition\Definition;
+use App\Model\CommandDefinition\Option;
+use PHPUnit\Framework\TestCase;
+
+class DefinitionTest extends TestCase
+{
+    /**
+     * @dataProvider buildDataProvider
+     */
+    public function testBuild(Definition $definition, string $expected): void
+    {
+        self::assertSame($expected, $definition->build());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function buildDataProvider(): array
+    {
+        return [
+            'no options, no arguments' => [
+                'definition' => new Definition('./command'),
+                'expected' => './command',
+            ],
+            'has options, no arguments' => [
+                'definition' => (new Definition('./command'))
+                    ->withOptions([
+                        Option::createShort('s'),
+                        Option::createLong('long')
+                    ]),
+                'expected' => './command -s --long',
+            ],
+            'no options, has arguments' => [
+                'definition' => (new Definition('./command'))
+                    ->withArguments([
+                        'one',
+                        'two',
+                    ]),
+                'expected' => "./command 'one' 'two'",
+            ],
+            'no options, has arguments containing single quotes' => [
+                'definition' => (new Definition('./command'))
+                    ->withArguments([
+                        "o'n'e",
+                        'two',
+                    ]),
+                'expected' => "./command 'o\\'n\\'e' 'two'",
+            ],
+            'has options, has arguments' => [
+                'definition' => (new Definition('./command'))
+                    ->withOptions([
+                        Option::createShort('s'),
+                        Option::createLong('long')
+                    ])->withArguments([
+                        'one',
+                        'two',
+                    ]),
+                'expected' => "./command -s --long 'one' 'two'",
+            ],
+        ];
+    }
+}
