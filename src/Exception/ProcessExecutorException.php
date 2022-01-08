11b22f5b05543cc99ee08df26e4bc38f9b2ab50b
Jon Cram <webignition@gmail.com>
2022-01-08 17:36:01 +0000

    Differentiate between symfony/process runtime exceptions (#174)

--- src/Exception/ProcessExecutorException.php
+++ src/Exception/ProcessExecutorException.php
@@ -12,26 +12,33 @@ use Symfony\Component\Process\Exception\RuntimeException;
 
 class ProcessExecutorException extends \Exception
 {
+    /**
+     * throw new RuntimeException(sprintf('The provided cwd "%s" does not exist.', $this->cwd));
+     * throw new RuntimeException('Unable to launch a new process.');.
+     */
     private const EXCEPTION_TYPE_TO_CODE_MAP = [
-        RuntimeException::class => 100,
         ProcessTimedOutException::class => 200,
         ProcessSignaledException::class => 300,
         LogicException::class => 400,
     ];
 
     private const EXCEPTION_TYPE_TO_MESSAGE_MAP = [
-        RuntimeException::class => 'Process cannot be started or is already running',
         ProcessTimedOutException::class => 'Process timed out',
         ProcessSignaledException::class => 'Process stopped after receiving signal',
         LogicException::class => 'Callback was provided and output has been disabled',
+    ];
+
+    private const RUNTIME_EXCEPTION_CODE_MAP = [
+        'Unable to launch' => 100,
+        'The provided cwd' => 101,
     ];
 
     public function __construct(
         private ExceptionInterface $previous
     ) {
         parent::__construct(
-            self::EXCEPTION_TYPE_TO_MESSAGE_MAP[$previous::class] ?? 'unknown',
-            self::EXCEPTION_TYPE_TO_CODE_MAP[$previous::class] ?? 0,
+            $this->getExceptionMessage($previous),
+            $this->getExceptionCode($previous),
             $previous
         );
     }
@@ -39,5 +46,39 @@ class ProcessExecutorException extends \Exception
     public function getException(): ExceptionInterface
     {
         return $this->previous;
+    }
+
+    private function getExceptionMessage(\Throwable $exception): string
+    {
+        $code = self::EXCEPTION_TYPE_TO_MESSAGE_MAP[$exception::class] ?? null;
+        if (is_string($code)) {
+            return $code;
+        }
+
+        if ($exception instanceof RuntimeException) {
+            return $exception->getMessage();
+        }
+
+        return 'unknown';
+    }
+
+    private function getExceptionCode(\Throwable $exception): int
+    {
+        $code = self::EXCEPTION_TYPE_TO_CODE_MAP[$exception::class] ?? null;
+        if (is_int($code)) {
+            return $code;
+        }
+
+        if (RuntimeException::class === $exception::class) {
+            $runtimeExceptionMessage = $exception->getMessage();
+
+            foreach (self::RUNTIME_EXCEPTION_CODE_MAP as $messageStartsWith => $exceptionCode) {
+                if (str_starts_with($runtimeExceptionMessage, $messageStartsWith)) {
+                    return $exceptionCode;
+                }
+            }
+        }
+
+        return 0;
     }
 }
