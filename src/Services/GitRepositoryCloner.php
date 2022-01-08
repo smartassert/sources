c0564714a284c85fab7f9fdd59ddd4464548dc99
Jon Cram <webignition@gmail.com>
2022-01-08 18:22:34 +0000

    Define command definition options and arguments (#177)

--- src/Services/GitRepositoryCloner.php
+++ src/Services/GitRepositoryCloner.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Exception\ProcessExecutorException;
-use App\Model\CommandDefinition;
+use App\Model\CommandDefinition\Definition;
+use App\Model\CommandDefinition\Option;
 use App\Model\ProcessOutput;
 use App\Services\Process\Executor;
 
@@ -21,12 +22,12 @@ class GitRepositoryCloner
      */
     public function clone(string $url, string $path): ProcessOutput
     {
-        return $this->processExecutor->execute(new CommandDefinition(
-            'git clone --no-checkout %url% %path%',
-            [
-                '%url%' => $url,
-                '%path%' => $path,
-            ]
-        ));
+        return $this->processExecutor->execute(
+            (new Definition('git clone'))
+                ->withOptions([
+                    Option::createLong('no-checkout'),
+                ])
+                ->withArguments([$url, $path])
+        );
     }
 }
