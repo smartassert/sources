c62ce828dd8fa833260039bd309c86a8c1b6b2b6
Jon Cram <webignition@gmail.com>
2022-01-10 15:33:21 +0000

    squash

--- src/Exception/FilePath/NonAbsolutePathException.php
+++ src/Exception/FilePath/NonAbsolutePathException.php
@@ -6,10 +6,8 @@ namespace App\Exception\FilePath;
 
 class NonAbsolutePathException extends AbstractFilePathException
 {
-    public function __construct(
-        string $path,
-        ?\Throwable $previous = null
-    ) {
+    public function __construct(string $path, ?\Throwable $previous = null)
+    {
         parent::__construct($path, sprintf('Path "%s" is not absolute"', $path), $previous);
     }
 }
