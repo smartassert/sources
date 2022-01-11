71af15a80c133ea262fecd6b52cd56a081942a0d
Jon Cram <webignition@gmail.com>
2022-01-11 09:30:49 +0000

    Revert "FileStoreManager methods take AbsoluteFileLocator (#186)"
    This reverts commit ef79bb8028f5f36a7ec57ac45f6c909f5606cd83.

--- tests/Functional/Services/FileStoreManagerTest.php
+++ tests/Functional/Services/FileStoreManagerTest.php
@@ -8,7 +8,6 @@ use App\Entity\GitSource;
 use App\Entity\RunSource;
 use App\Model\UserGitRepository;
 use App\Services\FileStoreManager;
-use App\Services\FileStorePathFactory;
 use App\Tests\Mock\Model\MockFileLocator;
 use App\Tests\Model\UserId;
 use App\Tests\Services\FileStoreFixtureCreator;
@@ -17,7 +16,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class FileStoreManagerTest extends WebTestCase
 {
     private FileStoreManager $fileStoreManager;
-    private FileStorePathFactory $fileStorePathFactory;
     private FileStoreFixtureCreator $fixtureCreator;
 
     protected function setUp(): void
@@ -28,10 +26,6 @@ class FileStoreManagerTest extends WebTestCase
         \assert($fileStoreManager instanceof FileStoreManager);
         $this->fileStoreManager = $fileStoreManager;
 
-        $fileStorePathFactory = self::getContainer()->get(FileStorePathFactory::class);
-        \assert($fileStorePathFactory instanceof FileStorePathFactory);
-        $this->fileStorePathFactory = $fileStorePathFactory;
-
         $fixtureCreator = self::getContainer()->get(FileStoreFixtureCreator::class);
         \assert($fixtureCreator instanceof FileStoreFixtureCreator);
         $this->fixtureCreator = $fixtureCreator;
@@ -39,12 +33,7 @@ class FileStoreManagerTest extends WebTestCase
 
     public function testExistsInitializeRemoveSuccess(): void
     {
-        $fileLocator = $this->fileStorePathFactory->create(
-            (new MockFileLocator())
-                ->withToStringCall(UserId::create())
-                ->getMock()
-        );
-
+        $fileLocator = (new MockFileLocator())->withToStringCall(UserId::create())->getMock();
         self::assertFalse($this->fileStoreManager->exists($fileLocator));
 
         $this->fileStoreManager->initialize($fileLocator);
@@ -58,10 +47,7 @@ class FileStoreManagerTest extends WebTestCase
     {
         $userId = UserId::create();
         $gitSource = new GitSource($userId, 'https://example.com/repository.git');
-
-        $sourceFileLocator = $this->fileStorePathFactory->create(
-            new UserGitRepository($gitSource)
-        );
+        $sourceFileLocator = new UserGitRepository($gitSource);
         self::assertFalse($this->fileStoreManager->exists($sourceFileLocator));
 
         $this->fileStoreManager->initialize($sourceFileLocator);
@@ -69,14 +55,14 @@ class FileStoreManagerTest extends WebTestCase
 
         $this->fixtureCreator->copyFixturesTo((string) $sourceFileLocator);
 
-        $targetFileLocator = $this->fileStorePathFactory->create(new RunSource($gitSource));
+        $targetFileLocator = new RunSource($gitSource);
         self::assertFalse($this->fileStoreManager->exists($targetFileLocator));
 
         $this->fileStoreManager->mirror($sourceFileLocator, $targetFileLocator);
 
         self::assertSame(
-            scandir((string) $sourceFileLocator),
-            scandir((string) $targetFileLocator)
+            scandir((string) $this->fileStoreManager->createPath($sourceFileLocator)),
+            scandir((string) $this->fileStoreManager->createPath($targetFileLocator))
         );
     }
 }
