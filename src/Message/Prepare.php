8c90c3fddc94040771f125a0bb05e784ca007e82
Jon Cram <webignition@gmail.com>
2022-02-01 14:39:32 +0000

    Include arbitrary string parameters in Prepare message (#240)

--- src/Message/Prepare.php
+++ src/Message/Prepare.php
@@ -6,13 +6,25 @@ namespace App\Message;
 
 class Prepare
 {
+    /**
+     * @param array<string, string> $parameters
+     */
     public function __construct(
-        private string $sourceId
+        private string $sourceId,
+        private array $parameters,
     ) {
     }
 
     public function getSourceId(): string
     {
         return $this->sourceId;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function getParameters(): array
+    {
+        return $this->parameters;
     }
 }
