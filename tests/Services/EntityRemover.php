4d63f1487dc5eb6e68536472a18af144cd13981f
Jon Cram <webignition@gmail.com>
2022-02-02 12:23:44 +0000

    Remove RunSourcePreparation entity

--- tests/Services/EntityRemover.php
+++ tests/Services/EntityRemover.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tests\Services;
 
-use App\Repository\RunSourcePreparationRepository;
 use App\Repository\SourceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -12,17 +11,12 @@ class EntityRemover
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private RunSourcePreparationRepository $runSourcePreparationRepository,
         private SourceRepository $sourceRepository,
     ) {
     }
 
     public function removeAll(): void
     {
-        foreach ($this->runSourcePreparationRepository->findAll() as $entity) {
-            $this->entityManager->remove($entity);
-        }
-
         foreach ($this->sourceRepository->findAll() as $entity) {
             $this->entityManager->remove($entity);
         }
