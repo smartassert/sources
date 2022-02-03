b8a11ca49ab5da97fe850407d5dfe2314d2c00d0
Jon Cram <webignition@gmail.com>
2022-02-03 17:26:31 +0000

    Create argument resolver for App\Entity\RunSource

---
+++ src/ArgumentResolver/RunSourceResolver.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ArgumentResolver;
+
+use App\Entity\RunSource;
+use App\Entity\SourceInterface;
+
+class RunSourceResolver extends AbstractSourceResolver
+{
+    protected function supportsArgumentType(string $type): bool
+    {
+        return RunSource::class === $type;
+    }
+
+    protected function doYield(?SourceInterface $source): ?SourceInterface
+    {
+        return $source instanceof RunSource ? $source : null;
+    }
+}
