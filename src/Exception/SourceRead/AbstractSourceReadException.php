92f1c8f9a9f15ee566c9686ff9b617e2e8336b88
Jon Cram <webignition@gmail.com>
2022-02-04 12:54:29 +0000

    Create RunSourceBuilder

---
+++ src/Exception/SourceRead/AbstractSourceReadException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception\SourceRead;
+
+use Symfony\Component\Finder\SplFileInfo;
+
+abstract class AbstractSourceReadException extends \Exception implements SourceReadExceptionInterface
+{
+    public function __construct(
+        private readonly SplFileInfo $sourceFile,
+        string $message,
+        int $code = 0,
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct(sprintf($message, $sourceFile->getPathname()), $code, $previous);
+    }
+
+    public function getSourceFile(): SplFileInfo
+    {
+        return $this->sourceFile;
+    }
+}
---
+++ src/Exception/SourceRead/ReadFileException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception\SourceRead;
+
+use Symfony\Component\Finder\SplFileInfo;
+
+class ReadFileException extends AbstractSourceReadException
+{
+    public function __construct(SplFileInfo $sourceFile)
+    {
+        parent::__construct($sourceFile, 'Unable to read file "%s"');
+    }
+}
