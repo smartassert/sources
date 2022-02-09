8736687fe3635189ec77d4167f1aa6eac4a8de7c
Jon Cram <webignition@gmail.com>
2022-02-09 17:24:15 +0000

    Unify file exceptions (#385)

---
+++ src/Exception/File/AbstractFileException.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exception\File;
+
+use League\Flysystem\FilesystemException;
+
+abstract class AbstractFileException extends \Exception implements FileExceptionInterface
+{
+    public function __construct(
+        string $action,
+        private string $path,
+        private FilesystemException $filesystemException,
+    ) {
+        parent::__construct(
+            sprintf('Unable to %s file "%s"', $action, $path),
+            0,
+            $filesystemException
+        );
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getFilesystemException(): FilesystemException
+    {
+        return $this->filesystemException;
+    }
+}
--- src/Exception/File/FileExceptionInterface.php
+++ src/Exception/File/FileExceptionInterface.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace App\Exception\File;
 
+use League\Flysystem\FilesystemException;
+
 interface FileExceptionInterface extends \Throwable
 {
+    public function getPath(): string;
+
+    public function getFilesystemException(): FilesystemException;
 }
