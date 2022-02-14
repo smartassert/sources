80a0513b34eed12428753ec191becdadffa2579f
Jon Cram <webignition@gmail.com>
2022-02-14 16:30:16 +0000

    Throw FileExceptionInterface instances in controllers (#407)

--- src/Controller/UserSourceController.php
+++ src/Controller/UserSourceController.php
@@ -7,14 +7,12 @@ namespace App\Controller;
 use App\Entity\OriginSourceInterface as OriginSource;
 use App\Entity\RunSource;
 use App\Entity\SourceInterface;
-use App\Exception\File\ReadException;
+use App\Exception\File\FileExceptionInterface;
 use App\Exception\InvalidRequestException;
 use App\Message\Prepare;
 use App\Request\SourceRequestInterface;
-use App\ResponseBody\FileExceptionResponse;
 use App\Security\UserSourceAccessChecker;
 use App\Services\RequestValidator;
-use App\Services\ResponseFactory;
 use App\Services\RunSourceFactory;
 use App\Services\RunSourceSerializer;
 use App\Services\Source\Mutator;
@@ -24,6 +22,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserSourceController
 {
@@ -32,6 +31,9 @@ class UserSourceController
     ) {
     }
 
+    /**
+     * @throws AccessDeniedException
+     */
     #[Route(SourceRoutes::ROUTE_SOURCE, name: 'user_source_get', methods: ['GET'])]
     public function get(SourceInterface $source): Response
     {
@@ -41,6 +43,7 @@ class UserSourceController
     }
 
     /**
+     * @throws AccessDeniedException
      * @throws InvalidRequestException
      */
     #[Route(SourceRoutes::ROUTE_SOURCE, name: 'user_source_update', methods: ['PUT'])]
@@ -56,6 +59,9 @@ class UserSourceController
         return new JsonResponse($mutator->update($source, $request));
     }
 
+    /**
+     * @throws AccessDeniedException
+     */
     #[Route(SourceRoutes::ROUTE_SOURCE, name: 'user_source_delete', methods: ['DELETE'])]
     public function delete(SourceInterface $source, Store $store): Response
     {
@@ -66,6 +72,9 @@ class UserSourceController
         return new JsonResponse();
     }
 
+    /**
+     * @throws AccessDeniedException
+     */
     #[Route(SourceRoutes::ROUTE_SOURCE . '/prepare', name: 'user_source_prepare', methods: ['POST'])]
     public function prepare(
         Request $request,
@@ -81,24 +90,17 @@ class UserSourceController
         return new JsonResponse($runSource, 202);
     }
 
+    /**
+     * @throws AccessDeniedException
+     * @throws FileExceptionInterface
+     */
     #[Route(SourceRoutes::ROUTE_SOURCE . '/read', name: 'user_source_read', methods: ['GET'])]
-    public function read(
-        RunSource $source,
-        RunSourceSerializer $runSourceSerializer,
-        ResponseFactory $responseFactory,
-    ): Response {
+    public function read(RunSource $source, RunSourceSerializer $runSourceSerializer): Response
+    {
         $this->userSourceAccessChecker->denyAccessUnlessGranted($source);
 
-        try {
-            return new Response(
-                $runSourceSerializer->read($source),
-                200,
-                [
-                    'content-type' => 'text/x-yaml; charset=utf-8',
-                ]
-            );
-        } catch (ReadException $exception) {
-            return $responseFactory->createErrorResponse(new FileExceptionResponse($exception), 500);
-        }
+        return new Response($runSourceSerializer->read($source), 200, [
+            'content-type' => 'text/x-yaml; charset=utf-8',
+        ]);
     }
 }
