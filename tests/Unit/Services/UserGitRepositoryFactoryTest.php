a10ae55d176f4ad318d425d2949b76a437ef81c9
Jon Cram <webignition@gmail.com>
2022-02-18 15:12:57 +0000

    Create UserGitRepositoryFactory

---
+++ tests/Unit/Services/UserGitRepositoryFactoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Services;
+
+use App\Entity\GitSource;
+use App\Model\UserGitRepository;
+use App\Services\UserGitRepositoryFactory;
+use App\Tests\Model\UserId;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserGitRepositoryFactoryTest extends WebTestCase
+{
+    public function testCreate(): void
+    {
+        $source = new GitSource(UserId::create(), 'http://example.com/repository.git');
+
+        $userGitRepository = (new UserGitRepositoryFactory())->create($source);
+
+        self::assertInstanceOf(UserGitRepository::class, $userGitRepository);
+        self::assertSame($source, $userGitRepository->getSource());
+    }
+}
