43cf7a0e01516083f9be8d45994361df4d73d2fb
Jon Cram <webignition@gmail.com>
2022-02-18 16:10:19 +0000

    Tidy up GitRepositoryStore

--- src/Services/GitRepositoryStore.php
+++ src/Services/GitRepositoryStore.php
@@ -7,7 +7,6 @@ namespace App\Services;
 use App\Entity\GitSource;
 use App\Exception\GitActionException;
 use App\Exception\GitRepositoryException;
-use App\Model\ProcessOutput;
 use App\Model\UserGitRepository;
 use Symfony\Component\String\UnicodeString;
 
@@ -30,53 +29,26 @@ class GitRepositoryStore
         $gitRepository = $this->gitRepositoryFactory->create($source);
         $relativePath = (string) $gitRepository;
 
-        try {
-            $this->gitRepositoryFileStore->remove($relativePath);
-        } catch (\Throwable $throwable) {
-            throw new GitRepositoryException($throwable);
-        }
-
         $absolutePath = $this->gitRepositoryPathFactory->createAbsolutePath($relativePath);
         $gitRepositoryUrl = $this->createRepositoryUrl($gitRepository->getSource());
 
         try {
-            $this->doCreate($gitRepositoryUrl, $absolutePath, $ref);
-        } catch (\Throwable $throwable) {
-            throw new GitRepositoryException($throwable);
+            $this->gitRepositoryFileStore->remove($relativePath);
+
+            $cloneOutput = $this->cloner->clone($gitRepositoryUrl, $absolutePath);
+            if (false === $cloneOutput->isSuccessful()) {
+                throw GitActionException::createFromCloneOutput($cloneOutput->getErrorOutput());
+            }
+
+            $checkoutOutput = $this->checkoutHandler->checkout($absolutePath, $ref);
+            if (false === $checkoutOutput->isSuccessful()) {
+                throw GitActionException::createFromCheckoutOutput($checkoutOutput->getErrorOutput());
+            }
+        } catch (\Throwable $e) {
+            throw new GitRepositoryException($e);
         }
 
         return $gitRepository;
-    }
-
-    /**
-     * @throws GitActionException
-     */
-    private function doCreate(string $gitRepositoryUrl, string $gitRepositoryAbsolutePath, ?string $ref): void
-    {
-        $gitActionException = null;
-        $cloneOutput = null;
-        $checkoutOutput = null;
-
-        try {
-            $cloneOutput = $this->cloner->clone($gitRepositoryUrl, $gitRepositoryAbsolutePath);
-
-            if ($cloneOutput->isSuccessful()) {
-                $checkoutOutput = $this->checkoutHandler->checkout($gitRepositoryAbsolutePath, $ref);
-            }
-        } catch (GitActionException $gitActionException) {
-        } finally {
-            if ($cloneOutput instanceof ProcessOutput && false === $cloneOutput->isSuccessful()) {
-                $gitActionException = GitActionException::createFromCloneOutput($cloneOutput->getErrorOutput());
-            }
-
-            if ($checkoutOutput instanceof ProcessOutput && false === $checkoutOutput->isSuccessful()) {
-                $gitActionException = GitActionException::createFromCheckoutOutput($checkoutOutput->getErrorOutput());
-            }
-
-            if ($gitActionException instanceof GitActionException) {
-                throw $gitActionException;
-            }
-        }
     }
 
     private function createRepositoryUrl(GitSource $source): string
