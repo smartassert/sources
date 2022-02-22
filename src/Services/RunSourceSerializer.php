05d3580f86bbb8b4b48e7b81fa01ba507777e618
Jon Cram <webignition@gmail.com>
2022-02-22 15:39:26 +0000

    Update RunSourceSerializer

--- src/Services/RunSourceSerializer.php
+++ src/Services/RunSourceSerializer.php
@@ -4,68 +4,53 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Entity\FileSource;
-use App\Entity\GitSource;
 use App\Entity\RunSource;
-use App\Exception\GitRepositoryException;
+use App\Exception\SerializableSourceCreationException;
+use App\Exception\SerializableSourceReaderNotFoundException;
+use App\Exception\UnparseableSourceFileException;
+use App\Exception\UnserializableSourceException;
+use App\Services\SerializableSource\Factory\Factory;
+use App\Services\SerializableSource\Serializer;
 use League\Flysystem\FilesystemException;
 use League\Flysystem\FilesystemReader;
 use League\Flysystem\FilesystemWriter;
-use Symfony\Component\Yaml\Exception\ParseException;
 
 class RunSourceSerializer
 {
     public const SERIALIZED_FILENAME = 'source.yaml';
 
     public function __construct(
-        private SourceSerializer $sourceSerializer,
-        private GitRepositoryStore $gitRepositoryStore,
-        private SerializableSourceLister $sourceLister,
-        private FilesystemReader $fileSourceReader,
-        private FilesystemReader $gitRepositoryReader,
-        private FilesystemWriter $gitRepositoryWriter,
+        private Serializer $serializer,
         private FilesystemReader $runSourceReader,
         private FilesystemWriter $runSourceWriter,
+        private Factory $serializableSourceFactory,
     ) {
     }
 
     /**
-     * @throws ParseException
      * @throws FilesystemException
-     * @throws GitRepositoryException
+     * @throws UnserializableSourceException
+     * @throws SerializableSourceCreationException
+     * @throws UnparseableSourceFileException
+     * @throws SerializableSourceReaderNotFoundException
      */
-    public function write(RunSource $target): void
+    public function write(RunSource $target): ?string
     {
         $source = $target->getParent();
-        $serializedSourcePath = $target . '/' . self::SERIALIZED_FILENAME;
-
-        $content = null;
-
-        if ($source instanceof FileSource) {
-            $files = $this->sourceLister->list($this->fileSourceReader, (string) $source);
-            $content = $this->sourceSerializer->serialize($this->fileSourceReader, $files);
+        if (null === $source) {
+            return null;
         }
 
-        if ($source instanceof GitSource) {
-            $gitRepository = $this->gitRepositoryStore->initialize($source, $target->getParameters()['ref'] ?? null);
-
-            $sourcePath = rtrim(
-                sprintf('%s/%s', $gitRepository, ltrim($source->getPath(), '/')),
-                '/'
-            );
-
-            $files = $this->sourceLister->list($this->gitRepositoryReader, $sourcePath);
-            $content = $this->sourceSerializer->serialize($this->gitRepositoryReader, $files);
-
-            try {
-                $this->gitRepositoryWriter->deleteDirectory((string) $gitRepository);
-            } catch (FilesystemException) {
-            }
+        $serializableSource = $this->serializableSourceFactory->create($source, $target->getParameters());
+        if (null === $serializableSource) {
+            throw new UnserializableSourceException($source);
         }
 
-        if (is_string($content)) {
-            $this->runSourceWriter->write($serializedSourcePath, $content);
-        }
+        $targetPath = $target . '/' . self::SERIALIZED_FILENAME;
+        $this->runSourceWriter->write($targetPath, $this->serializer->serialize($serializableSource));
+        $this->serializableSourceFactory->remove($serializableSource);
+
+        return $targetPath;
     }
 
     /**
