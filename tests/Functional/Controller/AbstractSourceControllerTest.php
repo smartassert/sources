c519123ae1a348e284c664dde1445631ddb6128b
Jon Cram <webignition@gmail.com>
2022-02-24 16:19:55 +0000

    Define AbstractSourceControllerTest::authenticatedUserId, set in setUp()

--- tests/Functional/Controller/AbstractSourceControllerTest.php
+++ tests/Functional/Controller/AbstractSourceControllerTest.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Controller;
 
 use App\Tests\Services\ApplicationClient;
+use App\Tests\Services\AuthenticationConfiguration;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Routing\RouterInterface;
 
 abstract class AbstractSourceControllerTest extends WebTestCase
 {
     protected const AUTHENTICATED_USER_ID = '01FWKGED9E2NZZR5HP21HS2YYT';
+    protected const AUTHENTICATED_USER_ID_PLACEHOLDER = '{{ authenticated_user_id }}';
 
     protected ApplicationClient $applicationClient;
+    protected string $authenticatedUserId = '';
     private RouterInterface $router;
 
     protected function setUp(): void
@@ -29,6 +32,10 @@ abstract class AbstractSourceControllerTest extends WebTestCase
         $router = self::getContainer()->get(RouterInterface::class);
         \assert($router instanceof RouterInterface);
         $this->router = $router;
+
+        $authenticationConfiguration = self::getContainer()->get(AuthenticationConfiguration::class);
+        \assert($authenticationConfiguration instanceof AuthenticationConfiguration);
+        $this->authenticatedUserId = $authenticationConfiguration->authenticatedUserId;
     }
 
     /**
