ba70ee7cf94a3e63b94f681eb750adf8d866abbd
Jon Cram <webignition@gmail.com>
2022-03-10 15:33:29 +0000

    Use file- and run-source fixtures in integration prepare tests (#654)

--- tests/Integration/PrepareSourceTest.php
+++ tests/Integration/PrepareSourceTest.php
@@ -4,31 +4,51 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration;
 
-use App\Entity\FileSource;
 use App\Enum\RunSource\State;
 use App\Enum\Source\Type;
+use App\Services\FileLister;
 use App\Tests\Application\AbstractPrepareSourceTest;
+use App\Tests\Services\SourceProvider;
+use League\Flysystem\FilesystemOperator;
 
 class PrepareSourceTest extends AbstractPrepareSourceTest
 {
     use GetClientAdapterTrait;
 
+    private FilesystemOperator $fixtureStorage;
+    private FileLister $fileLister;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $fixtureStorage = self::getContainer()->get('test_fixtures.storage');
+        \assert($fixtureStorage instanceof FilesystemOperator);
+        $this->fixtureStorage = $fixtureStorage;
+
+        $fileLister = self::getContainer()->get(FileLister::class);
+        \assert($fileLister instanceof FileLister);
+        $this->fileLister = $fileLister;
+    }
+
     public function testPrepareFileSource(): void
     {
-        $fileSource = new FileSource($this->authenticationConfiguration->authenticatedUserId, 'file source label');
-        $this->store->add($fileSource);
+        $this->sourceProvider->initialize([SourceProvider::FILE_WITHOUT_RUN_SOURCE]);
+        $fileSource = $this->sourceProvider->get(SourceProvider::FILE_WITHOUT_RUN_SOURCE);
 
-        $filename = 'filename.yaml';
-        $content = '- file content';
+        $sourceIdentifier = 'Source/yaml_valid';
+        $sourceFiles = $this->fileLister->list($this->fixtureStorage, $sourceIdentifier);
 
-        $addFileResponse = $this->applicationClient->makeAddFileRequest(
-            $this->authenticationConfiguration->validToken,
-            $fileSource->getId(),
-            $filename,
-            $content
-        );
+        foreach ($sourceFiles as $sourceFilePath) {
+            $addFileResponse = $this->applicationClient->makeAddFileRequest(
+                $this->authenticationConfiguration->validToken,
+                $fileSource->getId(),
+                $sourceFilePath,
+                trim($this->fixtureStorage->read($sourceIdentifier . '/' . $sourceFilePath))
+            );
 
-        $this->responseAsserter->assertSuccessfulResponseWithNoBody($addFileResponse);
+            $this->responseAsserter->assertSuccessfulResponseWithNoBody($addFileResponse);
+        }
 
         $prepareResponse = $this->applicationClient->makePrepareSourceRequest(
             $this->authenticationConfiguration->validToken,
@@ -60,10 +80,7 @@ class PrepareSourceTest extends AbstractPrepareSourceTest
             $runSourceId
         );
 
-        $expectedReadResponseBody = <<< 'EOF'
-            "filename.yaml": |
-              - file content
-            EOF;
+        $expectedReadResponseBody = trim($this->fixtureStorage->read('RunSource/source_yaml_entire.yaml'));
 
         $this->responseAsserter->assertReadSourceSuccessResponse($readResponse, $expectedReadResponseBody);
     }
