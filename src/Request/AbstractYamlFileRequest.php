5ed001c3f7315d2b4c34ceb9a0578e2ebfa63d59
Jon Cram <webignition@gmail.com>
2022-03-15 11:34:01 +0000

    Use smartassert/yaml-file 1.0 (#676)

--- src/Request/AbstractYamlFileRequest.php
+++ src/Request/AbstractYamlFileRequest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Request;
 
 use App\Validator\YamlFilenameConstraint;
-use SmartAssert\YamlFile\Model\Filename;
+use SmartAssert\YamlFile\Filename;
 use Symfony\Component\HttpFoundation\Request;
 use webignition\EncapsulatingRequestResolverBundle\Model\EncapsulatingRequestInterface;
 
