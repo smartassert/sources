17a9da10cd9ae91ba656bb071e8c50f5698bf615
Jon Cram <webignition@gmail.com>
2022-04-12 10:38:41 +0100

    Use health-check-bundle 0.3

--- src/Services/ServiceStatusInspector/MessageQueueInspector.php
+++ src/Services/ServiceStatusInspector/MessageQueueInspector.php
@@ -5,19 +5,29 @@ declare(strict_types=1);
 namespace App\Services\ServiceStatusInspector;
 
 use App\Message\Prepare;
+use SmartAssert\ServiceStatusInspector\ComponentInspectorInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
-class MessageQueueInspector
+class MessageQueueInspector implements ComponentInspectorInterface
 {
     public const INVALID_MACHINE_ID = 'intentionally invalid';
+    public const DEFAULT_IDENTIFIER = 'message_queue';
 
     public function __construct(
-        private MessageBusInterface $messageBus,
+        private readonly MessageBusInterface $messageBus,
+        private readonly string $identifier = self::DEFAULT_IDENTIFIER,
     ) {
     }
 
-    public function __invoke(): void
+    public function isAvailable(): bool
     {
         $this->messageBus->dispatch(new Prepare('invalid', []));
+
+        return true;
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
     }
 }
