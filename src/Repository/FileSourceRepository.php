085f0f383e6141627fa012b880cc6458ba5e4514
Jon Cram <webignition@gmail.com>
2023-02-06 17:48:57 +0000

    Add FileRepository::findOneFileSourceByUserAndLabel()

--- src/Repository/FileSourceRepository.php
+++ src/Repository/FileSourceRepository.php
@@ -6,7 +6,9 @@ namespace App\Repository;
 
 use App\Entity\FileSource;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method null|FileSource find($id, $lockMode = null, $lockVersion = null)
@@ -21,5 +23,29 @@ class FileSourceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, FileSource::class);
+    }
+
+    /**
+     * @param non-empty-string $label
+     */
+    public function findOneFileSourceByUserAndLabel(UserInterface $user, string $label): ?FileSource
+    {
+        $queryBuilder = $this->createQueryBuilder('FileSource')
+            ->where('FileSource.userId = :UserId')
+            ->andWhere('FileSource.label = :Label')
+            ->setParameter('UserId', $user->getUserIdentifier())
+            ->setParameter('Label', $label)
+            ->setMaxResults(1)
+        ;
+
+        $query = $queryBuilder->getQuery();
+
+        try {
+            $fileSource = $query->getOneOrNullResult();
+        } catch (NonUniqueResultException $e) {
+            return null;
+        }
+
+        return $fileSource instanceof FileSource ? $fileSource : null;
     }
 }
