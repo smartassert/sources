f6f8279dc27083a5e0d7377352a960037bcec6ab
Jon Cram <webignition@gmail.com>
2023-02-07 16:22:54 +0000

    Remove FileSourceRequest::getType(), GitSourceRequest::getType() (#746)

--- src/Request/FileSourceRequest.php
+++ src/Request/FileSourceRequest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Request;
 
 use App\Entity\FileSource;
-use App\Enum\Source\Type;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -21,11 +20,6 @@ class FileSourceRequest
         $payload = $request->request;
 
         $this->label = trim((string) $payload->get(self::PARAMETER_LABEL));
-    }
-
-    public function getType(): string
-    {
-        return Type::FILE->value;
     }
 
     public function getLabel(): string
