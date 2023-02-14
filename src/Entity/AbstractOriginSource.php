3b3081195b08caced3f8708c9525822c78e56a09
Jon Cram <webignition@gmail.com>
2023-02-14 10:21:10 +0000

    FileSource extends AbstractOriginSource

--- src/Entity/AbstractOriginSource.php
+++ src/Entity/AbstractOriginSource.php
@@ -11,15 +11,11 @@ abstract class AbstractOriginSource extends AbstractSource
 {
     public const LABEL_MAX_LENGTH = 255;
 
-    /**
-     * @var non-empty-string
-     */
     #[ORM\Column(type: 'string', length: self::LABEL_MAX_LENGTH)]
     private string $label;
 
     /**
      * @param non-empty-string $userId
-     * @param non-empty-string $label
      */
     public function __construct(string $userId, string $label)
     {
@@ -28,17 +24,11 @@ abstract class AbstractOriginSource extends AbstractSource
         $this->label = $label;
     }
 
-    /**
-     * @return non-empty-string
-     */
     public function getLabel(): string
     {
         return $this->label;
     }
 
-    /**
-     * @param non-empty-string $label
-     */
     public function setLabel(string $label): self
     {
         $this->label = $label;
