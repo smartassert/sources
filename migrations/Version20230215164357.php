1bf6a287b0a9e8f1f161569b6e04b70ce4d2fc00
Jon Cram <webignition@gmail.com>
2023-02-15 17:00:59 +0000

    Create suite entity (#803)

---
+++ migrations/Version20230215164357.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\Suite;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230215164357 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create table for ' . Suite::class;
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE suite (
+                id VARCHAR(32) NOT NULL,
+                source_id VARCHAR(32) NOT NULL,
+                user_id VARCHAR(32) NOT NULL,
+                label VARCHAR(255) NOT NULL,
+                tests TEXT NOT NULL,
+                PRIMARY KEY(id)
+           )
+       ');
+        $this->addSql('CREATE INDEX IDX_153CE426953C1C61 ON suite (source_id)');
+        $this->addSql('COMMENT ON COLUMN suite.tests IS \'(DC2Type:simple_array)\'');
+        $this->addSql('
+            ALTER TABLE suite 
+                ADD CONSTRAINT FK_153CE426953C1C61 
+                FOREIGN KEY (source_id) REFERENCES source (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE suite DROP CONSTRAINT FK_153CE426953C1C61');
+        $this->addSql('DROP TABLE suite');
+    }
+}
