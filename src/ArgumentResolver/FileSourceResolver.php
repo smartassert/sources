85ba6da976117fa6ee874a9e37e28ac1893004ae
Jon Cram <webignition@gmail.com>
2023-02-17 10:03:23 +0000

    Remove UnexpectedSourceTypeException (#832)

--- src/ArgumentResolver/FileSourceResolver.php
+++ src/ArgumentResolver/FileSourceResolver.php
@@ -5,9 +5,20 @@ declare(strict_types=1);
 namespace App\ArgumentResolver;
 
 use App\Entity\FileSource;
+use App\Repository\FileSourceRepository;
 
 class FileSourceResolver extends AbstractSingleSourceTypeResolver
 {
+    public function __construct(
+        private readonly FileSourceRepository $repository,
+    ) {
+    }
+
+    protected function find(string $id): ?FileSource
+    {
+        return $this->repository->find($id);
+    }
+
     protected function getSourceClassName(): string
     {
         return FileSource::class;
