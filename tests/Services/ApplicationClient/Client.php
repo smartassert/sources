743991dc39bed39e701cdc61bce33c179c585fbe
Jon Cram <webignition@gmail.com>
2023-02-17 09:52:53 +0000

    Get suite (#830)

--- tests/Services/ApplicationClient/Client.php
+++ tests/Services/ApplicationClient/Client.php
@@ -241,6 +241,18 @@ class Client
         );
     }
 
+    public function makeGetSuiteRequest(
+        ?string $authenticationToken,
+        string $sourceId,
+        string $suiteId
+    ): ResponseInterface {
+        return $this->client->makeRequest(
+            'GET',
+            $this->router->generate('user_suite_get', ['sourceId' => $sourceId, 'suiteId' => $suiteId]),
+            $this->createAuthorizationHeader($authenticationToken),
+        );
+    }
+
     /**
      * @return array<string, string>
      */
