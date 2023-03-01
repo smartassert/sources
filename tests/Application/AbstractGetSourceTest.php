3370e7a92c45183cbe0d86d998deb42d36fd6352
Jon Cram <webignition@gmail.com>
2023-03-01 11:55:07 +0000

    Remove usage in AbstractGetSourceTest

--- tests/Application/AbstractGetSourceTest.php
+++ tests/Application/AbstractGetSourceTest.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace App\Tests\Application;
 
+use App\Entity\FileSource;
+use App\Entity\GitSource;
 use App\Entity\RunSource;
+use App\Entity\SourceInterface;
 use App\Enum\RunSource\FailureReason;
 use App\Enum\RunSource\State;
 use App\Enum\Source\Type;
+use App\Repository\SourceRepository;
 use App\Services\EntityIdFactory;
-use App\Tests\Services\SourceProvider;
+use App\Tests\Services\AuthenticationConfiguration;
+use App\Tests\Services\FileSourceFactory;
+use App\Tests\Services\GitSourceFactory;
 
 abstract class AbstractGetSourceTest extends AbstractApplicationTest
 {
@@ -26,28 +32,23 @@ abstract class AbstractGetSourceTest extends AbstractApplicationTest
     /**
      * @dataProvider getSourceSuccessDataProvider
      *
-     * @param array<mixed> $expectedResponseData
+     * @param callable(AuthenticationConfiguration $authenticationConfiguration): SourceInterface $sourceCreator
+     * @param callable(SourceInterface $source): array<mixed> $expectedResponseDataCreator
      */
-    public function testGetSuccess(string $sourceIdentifier, array $expectedResponseData): void
+    public function testGetSuccess(callable $sourceCreator, callable $expectedResponseDataCreator): void
     {
-        $sourceProvider = self::getContainer()->get(SourceProvider::class);
-        \assert($sourceProvider instanceof SourceProvider);
-        $sourceProvider->setUserId(self::$authenticationConfiguration->getUser(self::USER_1_EMAIL)->id);
-        $sourceProvider->initialize();
+        $sourceRepository = self::getContainer()->get(SourceRepository::class);
+        \assert($sourceRepository instanceof SourceRepository);
 
-        $source = $sourceProvider->get($sourceIdentifier);
+        $source = $sourceCreator(self::$authenticationConfiguration);
+        $sourceRepository->save($source);
 
         $response = $this->applicationClient->makeGetSourceRequest(
             self::$authenticationConfiguration->getValidApiToken(self::USER_1_EMAIL),
             $source->getId()
         );
 
-        $expectedResponseData['id'] = $source->getId();
-        $expectedResponseData['user_id'] = $source->getUserId();
-
-        if ($source instanceof RunSource) {
-            $expectedResponseData['parent'] = $source->getParent()->getId();
-        }
+        $expectedResponseData = $expectedResponseDataCreator($source);
 
         $this->responseAsserter->assertSuccessfulJsonResponse($response, $expectedResponseData);
     }
@@ -59,49 +60,100 @@ abstract class AbstractGetSourceTest extends AbstractApplicationTest
     {
         return [
             'git source with credentials' => [
-                'sourceIdentifier' => SourceProvider::GIT_WITH_CREDENTIALS_WITH_RUN_SOURCE,
-                'expectedResponseData' => [
-                    'type' => Type::GIT->value,
-                    'label' => 'git source with credentials with run source',
-                    'host_url' => 'http://example.com/with-credentials.git',
-                    'path' => '/',
-                    'has_credentials' => true,
-                ],
+                'sourceCreator' => function (AuthenticationConfiguration $authenticationConfiguration) {
+                    return GitSourceFactory::create(
+                        userId: $authenticationConfiguration->getUser(self::USER_1_EMAIL)->id,
+                        credentials: md5((string) rand()),
+                    );
+                },
+                'expectedResponseDataCreator' => function (GitSource $source) {
+                    return [
+                        'id' => $source->getId(),
+                        'user_id' => $source->getUserId(),
+                        'type' => Type::GIT->value,
+                        'label' => $source->getLabel(),
+                        'host_url' => $source->getHostUrl(),
+                        'path' => $source->getPath(),
+                        'has_credentials' => true,
+                    ];
+                },
             ],
             'git source without credentials' => [
-                'sourceIdentifier' => SourceProvider::GIT_WITHOUT_CREDENTIALS_WITHOUT_RUN_SOURCE,
-                'expectedResponseData' => [
-                    'type' => Type::GIT->value,
-                    'label' => 'git source without credentials without run source',
-                    'host_url' => 'http://example.com/without-credentials.git',
-                    'path' => '/',
-                    'has_credentials' => false,
-                ],
+                'sourceCreator' => function (AuthenticationConfiguration $authenticationConfiguration) {
+                    return GitSourceFactory::create(
+                        userId: $authenticationConfiguration->getUser(self::USER_1_EMAIL)->id,
+                    );
+                },
+                'expectedResponseDataCreator' => function (GitSource $source) {
+                    return [
+                        'id' => $source->getId(),
+                        'user_id' => $source->getUserId(),
+                        'type' => Type::GIT->value,
+                        'label' => $source->getLabel(),
+                        'host_url' => $source->getHostUrl(),
+                        'path' => $source->getPath(),
+                        'has_credentials' => false,
+                    ];
+                },
             ],
-            'file with run source' => [
-                'sourceIdentifier' => SourceProvider::FILE_WITH_RUN_SOURCE,
-                'expectedResponseData' => [
-                    'type' => Type::FILE->value,
-                    'label' => 'with run source',
-                ],
+            'file' => [
+                'sourceCreator' => function (AuthenticationConfiguration $authenticationConfiguration) {
+                    return FileSourceFactory::create(
+                        userId: $authenticationConfiguration->getUser(self::USER_1_EMAIL)->id,
+                    );
+                },
+                'expectedResponseDataCreator' => function (FileSource $source) {
+                    return [
+                        'id' => $source->getId(),
+                        'user_id' => $source->getUserId(),
+                        'type' => Type::FILE->value,
+                        'label' => $source->getLabel(),
+                    ];
+                },
             ],
             'run' => [
-                'sourceIdentifier' => SourceProvider::RUN_WITH_FILE_PARENT,
-                'expectedResponseData' => [
-                    'type' => Type::RUN->value,
-                    'parameters' => [],
-                    'state' => State::REQUESTED->value,
-                ],
+                'sourceCreator' => function (AuthenticationConfiguration $authenticationConfiguration) {
+                    $parent = FileSourceFactory::create(
+                        userId: $authenticationConfiguration->getUser(self::USER_1_EMAIL)->id
+                    );
+
+                    return new RunSource((new EntityIdFactory())->create(), $parent);
+                },
+                'expectedResponseDataCreator' => function (RunSource $source) {
+                    return [
+                        'id' => $source->getId(),
+                        'user_id' => $source->getUserId(),
+                        'type' => Type::RUN->value,
+                        'parameters' => [],
+                        'state' => State::REQUESTED->value,
+                    ];
+                },
             ],
-            'run: preparation failed' => [
-                'sourceIdentifier' => SourceProvider::RUN_FAILED,
-                'expectedResponseData' => [
-                    'type' => Type::RUN->value,
-                    'parameters' => [],
-                    'state' => State::FAILED->value,
-                    'failure_reason' => FailureReason::GIT_CLONE->value,
-                    'failure_message' => 'fatal: repository \'http://example.com/with-credentials.git\' not found',
-                ],
+            'run, preparation failed' => [
+                'sourceCreator' => function (AuthenticationConfiguration $authenticationConfiguration) {
+                    $parent = FileSourceFactory::create(
+                        userId: $authenticationConfiguration->getUser(self::USER_1_EMAIL)->id
+                    );
+
+                    $source = new RunSource((new EntityIdFactory())->create(), $parent);
+                    $source->setPreparationFailed(
+                        FailureReason::GIT_CLONE,
+                        'fatal: repository \'http://example.com/with-credentials.git\' not found'
+                    );
+
+                    return $source;
+                },
+                'expectedResponseDataCreator' => function (RunSource $source) {
+                    return [
+                        'id' => $source->getId(),
+                        'user_id' => $source->getUserId(),
+                        'type' => Type::RUN->value,
+                        'parameters' => [],
+                        'state' => State::FAILED->value,
+                        'failure_reason' => FailureReason::GIT_CLONE->value,
+                        'failure_message' => 'fatal: repository \'http://example.com/with-credentials.git\' not found',
+                    ];
+                },
             ],
         ];
     }
