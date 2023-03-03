658b71edf7c87c7f8556cc5acba1da29fb9693cd
Jon Cram <webignition@gmail.com>
2023-03-03 17:34:25 +0000

    Create test suite factory (#919)

---
+++ tests/Services/SuiteFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Services;
+
+use App\Entity\SourceOriginInterface;
+use App\Entity\Suite;
+use App\Services\EntityIdFactory;
+
+class SuiteFactory
+{
+    /**
+     * @param null|non-empty-string   $label
+     * @param null|non-empty-string[] $tests
+     */
+    public static function create(
+        SourceOriginInterface $source,
+        ?string $label = null,
+        ?array $tests = null,
+    ): Suite {
+        $suite = new Suite((new EntityIdFactory())->create(), $source);
+
+        $label = is_string($label) ? $label : md5((string) rand());
+        $suite->setLabel($label);
+
+        $tests = is_array($tests) ? $tests : [];
+        $suite->setTests($tests);
+
+        return $suite;
+    }
+}
