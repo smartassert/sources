090bca85be8fb513f5ef15e327acac53a9db4ccd
Jon Cram <webignition@gmail.com>
2023-03-03 15:00:25 +0000

    Update source routes to be source-specific (#911)

--- src/Controller/SourceRoutes.php
+++ src/Controller/SourceRoutes.php
@@ -7,5 +7,5 @@ namespace App\Controller;
 class SourceRoutes
 {
     public const ROUTE_SOURCE_ID_PATTERN = '{sourceId<[A-Z90-9]{26}>}';
-    public const ROUTE_SOURCE = '/' . self::ROUTE_SOURCE_ID_PATTERN;
+    public const ROUTE_SOURCE = '/source/' . self::ROUTE_SOURCE_ID_PATTERN;
 }
