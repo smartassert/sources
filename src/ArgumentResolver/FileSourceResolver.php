148422febb419a3818512e78df0a99d62a7f281f
Jon Cram <webignition@gmail.com>
2023-03-10 15:30:58 +0000

    Remove source-type repository usage in source resolvers

--- src/ArgumentResolver/FileSourceResolver.php
+++ src/ArgumentResolver/FileSourceResolver.php
@@ -5,20 +5,9 @@ declare(strict_types=1);
 namespace App\ArgumentResolver;
 
 use App\Entity\FileSource;
-use App\Repository\FileSourceRepository;
 
 class FileSourceResolver extends AbstractSourceResolver
 {
-    public function __construct(
-        private readonly FileSourceRepository $repository,
-    ) {
-    }
-
-    protected function find(string $id): ?FileSource
-    {
-        return $this->repository->find($id);
-    }
-
     protected function supportsArgumentType(string $type): bool
     {
         return FileSource::class === $type;
