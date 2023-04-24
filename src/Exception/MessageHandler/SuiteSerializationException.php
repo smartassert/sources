5c38d0ebec9ee49ce3c45a16b3d67652a05f402d
Jon Cram <webignition@gmail.com>
2023-04-24 11:08:29 +0100

    SuiteSerializationException contains affected serialized suite

--- src/Exception/MessageHandler/SuiteSerializationException.php
+++ src/Exception/MessageHandler/SuiteSerializationException.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Exception\MessageHandler;
 
+use App\Entity\SerializedSuite;
+
 class SuiteSerializationException extends \Exception
 {
     public function __construct(
+        public readonly SerializedSuite $serializedSuite,
         private \Throwable $exception,
     ) {
         parent::__construct($exception->getMessage(), $exception->getCode(), $exception);
