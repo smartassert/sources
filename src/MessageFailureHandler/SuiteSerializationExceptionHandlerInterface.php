604d877c9f0fb36c29c67a2218c63c15f536353b
Jon Cram <webignition@gmail.com>
2023-04-26 11:17:44 +0100

    Handle FailureReason::GIT_CLONE (#1067)

---
+++ src/MessageFailureHandler/SuiteSerializationExceptionHandlerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MessageFailureHandler;
+
+use App\Entity\SerializedSuite;
+
+interface SuiteSerializationExceptionHandlerInterface
+{
+    public function handle(SerializedSuite $serializedSuite, \Throwable $exception): void;
+}
