c5833cd3419a678c07e6231ee261114843ab9c65
Jon Cram <webignition@gmail.com>
2023-04-27 17:40:12 +0100

    Remove unused FailureReason cases (#1092)

--- src/Enum/SerializedSuite/FailureReason.php
+++ src/Enum/SerializedSuite/FailureReason.php
@@ -7,10 +7,6 @@ namespace App\Enum\SerializedSuite;
 enum FailureReason: string
 {
     case UNKNOWN = 'unknown';
-    case TARGET_CREATE = 'target/create';
-    case TARGET_REMOVE = 'target/remove';
-    case TARGET_OUT_OF_SCOPE = 'target/out-of-scope';
-    case MIRROR = 'mirror';
     case GIT_REPOSITORY_OUT_OF_SCOPE = 'local-git-repository/out-of-scope';
     case GIT_CLONE = 'git/clone';
     case GIT_CHECKOUT = 'git/checkout';
