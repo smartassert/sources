245ca93e5b8be21e7ceae010e9c9397c03341cdb
Jon Cram <webignition@gmail.com>
2023-05-18 12:33:45 +0100

    Add nullable SerializeSuiteException::failureReason, failureMessage

--- src/Exception/MessageHandler/SerializeSuiteException.php
+++ src/Exception/MessageHandler/SerializeSuiteException.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Exception\MessageHandler;
 
 use App\Entity\SerializedSuite;
+use App\Enum\SerializedSuite\FailureReason;
 
 class SerializeSuiteException extends \Exception
 {
     public function __construct(
         public readonly SerializedSuite $serializedSuite,
         public readonly \Throwable $handlerException,
+        public readonly ?FailureReason $failureReason = null,
+        public readonly ?string $failureMessage = null,
     ) {
         parent::__construct($handlerException->getMessage(), $handlerException->getCode(), $handlerException);
     }
