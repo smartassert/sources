320d44bcd3a0ee86151bbc318b6f949646a225e5
Jon Cram <webignition@gmail.com>
2023-06-26 12:08:27 +0100

    Handle serialized suite access checks in value resolver

--- src/Controller/SerializedSuiteController.php
+++ src/Controller/SerializedSuiteController.php
@@ -10,7 +10,6 @@ use App\Exception\EmptyEntityIdException;
 use App\Exception\SerializedSuiteSourceDoesNotExistException;
 use App\Message\SerializeSuite;
 use App\Response\YamlResponse;
-use App\Security\EntityAccessChecker;
 use App\Services\Suite\SerializedSuiteFactory;
 use App\Services\SuiteSerializer;
 use League\Flysystem\FilesystemException;
@@ -19,15 +18,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class SerializedSuiteController
 {
-    public function __construct(
-        private readonly EntityAccessChecker $entityAccessChecker,
-    ) {
-    }
-
     /**
      * @throws EmptyEntityIdException
      */
@@ -46,7 +39,6 @@ class SerializedSuiteController
     }
 
     /**
-     * @throws AccessDeniedException
      * @throws FilesystemException
      */
     #[Route(SerializedSuiteRoutes::ROUTE_SERIALIZED_SUITE . '/read', name: 'serialized_suite_read', methods: ['GET'])]
@@ -54,8 +46,6 @@ class SerializedSuiteController
         SerializedSuite $serializedSuite,
         SuiteSerializer $suiteSerializer,
     ): Response {
-        $this->entityAccessChecker->denyAccessUnlessGranted($serializedSuite);
-
         try {
             return new YamlResponse($suiteSerializer->read($serializedSuite));
         } catch (SerializedSuiteSourceDoesNotExistException) {
@@ -63,14 +53,9 @@ class SerializedSuiteController
         }
     }
 
-    /**
-     * @throws AccessDeniedException
-     */
     #[Route(SerializedSuiteRoutes::ROUTE_SERIALIZED_SUITE, name: 'serialized_suite_get', methods: ['GET'])]
     public function get(SerializedSuite $serializedSuite): Response
     {
-        $this->entityAccessChecker->denyAccessUnlessGranted($serializedSuite);
-
         return new JsonResponse($serializedSuite);
     }
 }
