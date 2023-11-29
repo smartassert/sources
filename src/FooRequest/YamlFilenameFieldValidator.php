655f48a3a869874a44aad32686fff171a7429111
Jon Cram <webignition@gmail.com>
2023-11-29 14:16:14 +0000

    Use FooInvalidRequestException for bad yaml file request issues (#1240)

---
+++ src/FooRequest/YamlFilenameFieldValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FooRequest;
+
+use App\Exception\FooInvalidRequestException;
+use App\FooRequest\Field\YamlFilenameField;
+use SmartAssert\YamlFile\Filename as YamlFilename;
+use SmartAssert\YamlFile\Validator\YamlFilenameValidator;
+
+readonly class YamlFilenameFieldValidator
+{
+    public function __construct(
+        private YamlFilenameValidator $yamlFilenameValidator,
+    ) {
+    }
+
+    public function validate(YamlFilenameField $field): YamlFilename
+    {
+        $filename = $field->getFilename();
+        $validation = $this->yamlFilenameValidator->validate($filename);
+
+        if (false === $validation->isValid()) {
+            throw new FooInvalidRequestException('invalid_request_field', $field, 'invalid');
+        }
+
+        return $filename;
+    }
+}
