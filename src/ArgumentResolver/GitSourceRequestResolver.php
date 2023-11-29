a2147879bbd1bb2177e4cc53774a54e84b0b60f5
Jon Cram <webignition@gmail.com>
2023-11-29 11:28:55 +0000

    Factor sourcr request factories into source request resolvers (#1237)

--- src/ArgumentResolver/GitSourceRequestResolver.php
+++ src/ArgumentResolver/GitSourceRequestResolver.php
@@ -4,24 +4,27 @@ declare(strict_types=1);
 
 namespace App\ArgumentResolver;
 
-use App\Exception\InvalidRequestException;
+use App\Entity\AbstractSource;
+use App\Entity\GitSource;
+use App\Exception\FooInvalidRequestException;
+use App\FooRequest\Field\StringField;
+use App\FooRequest\StringFieldValidator;
 use App\Request\GitSourceRequest;
-use App\RequestFactory\GitSourceRequestFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-class GitSourceRequestResolver implements ValueResolverInterface
+readonly class GitSourceRequestResolver implements ValueResolverInterface
 {
     public function __construct(
-        private readonly GitSourceRequestFactory $gitSourceRequestFactory,
+        private StringFieldValidator $fieldValidator,
     ) {
     }
 
     /**
      * @return GitSourceRequest[]
      *
-     * @throws InvalidRequestException
+     * @throws FooInvalidRequestException
      */
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
@@ -29,6 +32,34 @@ class GitSourceRequestResolver implements ValueResolverInterface
             return [];
         }
 
-        return [$this->gitSourceRequestFactory->create($request)];
+        $label = $this->fieldValidator->validateNonEmptyString(new StringField(
+            GitSourceRequest::PARAMETER_LABEL,
+            trim($request->request->getString(GitSourceRequest::PARAMETER_LABEL)),
+            1,
+            AbstractSource::LABEL_MAX_LENGTH,
+        ));
+
+        $hostUrl = $this->fieldValidator->validateNonEmptyString(new StringField(
+            GitSourceRequest::PARAMETER_HOST_URL,
+            trim($request->request->getString(GitSourceRequest::PARAMETER_HOST_URL)),
+            1,
+            GitSource::HOST_URL_MAX_LENGTH,
+        ));
+
+        $path = $this->fieldValidator->validateNonEmptyString(new StringField(
+            GitSourceRequest::PARAMETER_PATH,
+            trim($request->request->getString(GitSourceRequest::PARAMETER_PATH)),
+            1,
+            GitSource::PATH_MAX_LENGTH,
+        ));
+
+        $credentials = $this->fieldValidator->validateString(new StringField(
+            GitSourceRequest::PARAMETER_CREDENTIALS,
+            trim($request->request->getString(GitSourceRequest::PARAMETER_CREDENTIALS)),
+            0,
+            GitSource::CREDENTIALS_MAX_LENGTH,
+        ));
+
+        return [new GitSourceRequest($label, $hostUrl, $path, $credentials)];
     }
 }
