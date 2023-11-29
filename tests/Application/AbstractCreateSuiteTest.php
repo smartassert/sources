6fa4762b9f44b8c9d2403b822c5a4067fc21408c
Jon Cram <webignition@gmail.com>
2023-11-29 10:39:39 +0000

    Apply tests to suite updation

--- tests/Application/AbstractCreateSuiteTest.php
+++ tests/Application/AbstractCreateSuiteTest.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace App\Tests\Application;
 
-use App\Entity\AbstractSource;
 use App\Entity\Suite;
 use App\Repository\SuiteRepository;
 use App\Request\SuiteRequest;
+use App\Tests\DataProvider\CreateUpdateSuiteDataProviderTrait;
 
 abstract class AbstractCreateSuiteTest extends AbstractSuiteTest
 {
+    use CreateUpdateSuiteDataProviderTrait;
+
     /**
-     * @dataProvider createSuiteInvalidRequestDataProvider
+     * @dataProvider createUpdateSuiteInvalidRequestDataProvider
      *
      * @param array<mixed> $requestParameters
      * @param array<mixed> $expectedResponseData
@@ -36,97 +38,6 @@ abstract class AbstractCreateSuiteTest extends AbstractSuiteTest
             (string) json_encode($expectedResponseData),
             $response->getBody()->getContents(),
         );
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public static function createSuiteInvalidRequestDataProvider(): array
-    {
-        $labelTooLong = str_repeat('.', AbstractSource::LABEL_MAX_LENGTH + 1);
-
-        return [
-            'missing label' => [
-                'requestParameters' => [],
-                'expectedResponseData' => [
-                    'class' => 'invalid_request_field',
-                    'field' => [
-                        'name' => 'label',
-                        'value' => '',
-                    ],
-                    'type' => 'empty',
-                    'requirements' => [
-                        'data_type' => 'string',
-                        'size' => [
-                            'minimum' => 1,
-                            'maximum' => 255,
-                        ]
-                    ],
-                ],
-            ],
-            'label length exceeds length limit' => [
-                'requestParameters' => [
-                    SuiteRequest::PARAMETER_LABEL => $labelTooLong,
-                ],
-                'expectedResponseData' => [
-                    'class' => 'invalid_request_field',
-                    'field' => [
-                        'name' => 'label',
-                        'value' => $labelTooLong,
-                    ],
-                    'type' => 'too_large',
-                    'requirements' => [
-                        'data_type' => 'string',
-                        'size' => [
-                            'minimum' => 1,
-                            'maximum' => 255,
-                        ]
-                    ],
-                ],
-            ],
-            'invalid yaml filename within singular tests collection' => [
-                'requestParameters' => [
-                    SuiteRequest::PARAMETER_LABEL => md5((string) rand()),
-                    SuiteRequest::PARAMETER_TESTS => ['test.txt'],
-                ],
-                'expectedResponseData' => [
-                    'class' => 'invalid_request_field',
-                    'field' => [
-                        'name' => 'tests',
-                        'position' => 1,
-                        'value' => [
-                            'test.txt',
-                        ],
-                    ],
-                    'type' => 'invalid',
-                    'requirements' => [
-                        'data_type' => 'yaml_filename_collection'
-                    ],
-                ],
-            ],
-            'invalid yaml filename within tests collection' => [
-                'requestParameters' => [
-                    SuiteRequest::PARAMETER_LABEL => md5((string) rand()),
-                    SuiteRequest::PARAMETER_TESTS => ['test.yaml', 'test.txt', 'test.yml'],
-                ],
-                'expectedResponseData' => [
-                    'class' => 'invalid_request_field',
-                    'field' => [
-                        'name' => 'tests',
-                        'position' => 2,
-                        'value' => [
-                            'test.yaml',
-                            'test.txt',
-                            'test.yml',
-                        ],
-                    ],
-                    'type' => 'invalid',
-                    'requirements' => [
-                        'data_type' => 'yaml_filename_collection'
-                    ],
-                ],
-            ],
-        ];
     }
 
     /**
