4540873f56c35ee13a8b0842e681de3537d8a012
Jon Cram <webignition@gmail.com>
2023-11-29 09:29:47 +0000

    Return valid string from field validator (#1233)

--- src/FooRequest/FieldValidator.php
+++ src/FooRequest/FieldValidator.php
@@ -12,21 +12,33 @@ class FieldValidator
     /**
      * @throws FooInvalidRequestException
      */
-    public function validate(FieldInterface $field): void
+    public function validateString(FieldInterface $field): string
     {
-        $value = $field->getValue();
-
-        if ('' === $value && !$field->getRequirements()->canBeEmpty()) {
-            throw new FooInvalidRequestException('invalid_request_field', $field, 'empty');
-        }
+        $value = (string) $field->getValue();
 
         $sizeRequirements = $field->getRequirements()->getSize();
         if ($sizeRequirements instanceof SizeInterface) {
-            if ($field instanceof StringFieldInterface && is_string($value)) {
-                if (mb_strlen($value) > $sizeRequirements->getMaximum()) {
-                    throw new FooInvalidRequestException('invalid_request_field', $field, 'too_large');
-                }
+            if (mb_strlen($value) > $sizeRequirements->getMaximum()) {
+                throw new FooInvalidRequestException('invalid_request_field', $field, 'too_large');
             }
         }
+
+        return $value;
+    }
+
+    /**
+     * @return non-empty-string
+     *
+     * @throws FooInvalidRequestException
+     */
+    public function validateNonEmptyString(FieldInterface $field): string
+    {
+        $value = $this->validateString($field);
+
+        if ('' === $value) {
+            throw new FooInvalidRequestException('invalid_request_field', $field, 'empty');
+        }
+
+        return $value;
     }
 }
