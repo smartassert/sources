398886a50d2cbc87c6c5c9f53460691e2375016d
Jon Cram <webignition@gmail.com>
2023-11-29 16:51:52 +0000

    Remove Remove ResponseFactory

--- src/EventListener/KernelExceptionEventSubscriber.php
+++ src/EventListener/KernelExceptionEventSubscriber.php
@@ -12,7 +12,6 @@ use App\FooResponse\ErrorInterface;
 use App\FooResponse\RenderableErrorInterface;
 use App\FooResponse\SizeInterface;
 use App\ResponseBody\FilesystemExceptionResponse;
-use App\Services\ResponseFactory;
 use League\Flysystem\FilesystemException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,11 +20,6 @@ use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
 readonly class KernelExceptionEventSubscriber implements EventSubscriberInterface
 {
-    public function __construct(
-        private ResponseFactory $responseFactory,
-    ) {
-    }
-
     /**
      * @return array<class-string, array<mixed>>
      */
@@ -68,7 +62,17 @@ readonly class KernelExceptionEventSubscriber implements EventSubscriberInterfac
 
     private function handleFilesystemException(FilesystemException $throwable): Response
     {
-        return $this->responseFactory->createErrorResponse(new FilesystemExceptionResponse($throwable), 500);
+        $error = new FilesystemExceptionResponse($throwable);
+
+        return new JsonResponse(
+            [
+                'error' => [
+                    'type' => $error->getType(),
+                    'payload' => $error->getPayload(),
+                ],
+            ],
+            500
+        );
     }
 
     private function handleFooHttpError(ErrorInterface $error): Response
