e0862389cd889cf5660e7d29a1a34f0862759f78
Jon Cram <webignition@gmail.com>
2023-11-30 17:32:15 +0000

    Implement for FilesystemException

--- src/EventListener/KernelExceptionEventSubscriber.php
+++ src/EventListener/KernelExceptionEventSubscriber.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
-use App\Exception\EntityStorageException;
 use App\Exception\HasHttpErrorCodeInterface;
 use App\FooRequest\CollectionFieldInterface;
 use App\FooRequest\RequirementsInterface;
 use App\FooRequest\ScalarRequirementsInterface;
 use App\FooResponse\BadRequestErrorInterface;
+use App\FooResponse\EntityErrorInterface;
 use App\FooResponse\ErrorInterface;
 use App\FooResponse\RenderableErrorInterface;
 use App\FooResponse\SizeInterface;
-use App\ResponseBody\FilesystemExceptionResponse;
-use League\Flysystem\FilesystemException;
-use League\Flysystem\FilesystemOperationFailed;
+use App\FooResponse\StorageLocationErrorInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,14 +46,6 @@ readonly class KernelExceptionEventSubscriber implements EventSubscriberInterfac
             $response = $this->handleFooHttpError($throwable);
         }
 
-        if ($throwable instanceof EntityStorageException) {
-            $response = $this->handleEntityStorageException($throwable);
-        }
-
-        if ($throwable instanceof FilesystemException) {
-            $response = $this->handleFilesystemException($throwable);
-        }
-
         if ($response instanceof Response) {
             $event->setResponse($response);
             $event->stopPropagation();
@@ -65,48 +55,6 @@ readonly class KernelExceptionEventSubscriber implements EventSubscriberInterfac
     private function handleHttpErrorException(HasHttpErrorCodeInterface $throwable): Response
     {
         return new Response(null, $throwable->getErrorCode());
-    }
-
-    private function handleFilesystemException(FilesystemException $throwable): Response
-    {
-        $error = new FilesystemExceptionResponse($throwable);
-
-        return new JsonResponse(
-            [
-                'error' => [
-                    'type' => $error->getType(),
-                    'payload' => $error->getPayload(),
-                ],
-            ],
-            500
-        );
-    }
-
-    private function handleEntityStorageException(EntityStorageException $throwable): Response
-    {
-        $filesystemException = $throwable->filesystemException;
-        $operationType = 'unknown';
-        if ($filesystemException instanceof FilesystemOperationFailed) {
-            $operationType = strtolower($filesystemException->operation());
-        }
-
-        $location = null;
-        if (method_exists($filesystemException, 'location')) {
-            $location = $filesystemException->location();
-        }
-
-        return new JsonResponse(
-            [
-                'class' => 'entity_storage',
-                'entity' => [
-                    'type' => $throwable->entity->getEntityType()->value,
-                    'id' => $throwable->entity->getId(),
-                ],
-                'type' => $operationType,
-                'location' => $location,
-            ],
-            500
-        );
     }
 
     private function handleFooHttpError(ErrorInterface $error): Response
@@ -157,6 +105,19 @@ readonly class KernelExceptionEventSubscriber implements EventSubscriberInterfac
         $type = $error->getType();
         if (is_string($type)) {
             $data['type'] = $type;
+        }
+
+        if ($error instanceof EntityErrorInterface) {
+            $entity = $error->getEntity();
+
+            $data['entity'] = [
+                'type' => $entity->getEntityType()->value,
+                'id' => $entity->getId(),
+            ];
+        }
+
+        if ($error instanceof StorageLocationErrorInterface) {
+            $data['location'] = $error->getLocation();
         }
 
         $statusCode = $error instanceof HasHttpErrorCodeInterface ? $error->getErrorCode() : 400;
