e666744223bbc9d3450c3bd3e84040d9c60d54d4
Jon Cram <webignition@gmail.com>
2023-12-04 10:40:54 +0000

    HasHttpErrorCodeInterface does not need to extend \Throwable

--- src/Exception/HasHttpErrorCodeInterface.php
+++ src/Exception/HasHttpErrorCodeInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Exception;
 
-interface HasHttpErrorCodeInterface extends \Throwable
+interface HasHttpErrorCodeInterface
 {
     public function getErrorCode(): int;
 }
