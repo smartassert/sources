8a862241e79669f994d8751916f3ca0b93f8b6f7
Jon Cram <webignition@gmail.com>
2023-12-04 09:57:21 +0000

    Simplify error component

--- src/FooResponse/ErrorSerializer/Component.php
+++ src/FooResponse/ErrorSerializer/Component.php
@@ -7,32 +7,8 @@ namespace App\FooResponse\ErrorSerializer;
 readonly class Component
 {
     public function __construct(
-        private string $key,
-        private mixed $data,
+        public string $key,
+        public mixed $data,
     ) {
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function toArray(): array
-    {
-        if (is_array($this->data)) {
-            $renderedData = [];
-
-            foreach ($this->data as $key => $value) {
-                if ($value instanceof Component) {
-                    $renderedData = array_merge($renderedData, $value->toArray());
-                } else {
-                    $renderedData[$key] = $value;
-                }
-            }
-        } else {
-            $renderedData = $this->data;
-        }
-
-        return [
-            $this->key => $renderedData,
-        ];
     }
 }
