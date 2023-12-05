7666cf1f881dbc114d6e61308f9ae08af0d26176
Jon Cram <webignition@gmail.com>
2023-12-05 11:27:24 +0000

    Add RequirementsInterface::getSize()

--- src/RequestField/Field/Requirements.php
+++ src/RequestField/Field/Requirements.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\RequestField\Field;
 
 use App\RequestField\RequirementsInterface;
+use App\RequestField\SizeInterface;
 
 readonly class Requirements implements RequirementsInterface
 {
@@ -13,11 +14,17 @@ readonly class Requirements implements RequirementsInterface
      */
     public function __construct(
         private string $dataType,
+        private ?SizeInterface $size = null,
     ) {
     }
 
     public function getDataType(): string
     {
         return $this->dataType;
+    }
+
+    public function getSize(): ?SizeInterface
+    {
+        return $this->size;
     }
 }
