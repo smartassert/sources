f123697aa486e671426ac2fb368ae86c92a7aaf0
Jon Cram <webignition@gmail.com>
2023-12-13 11:18:50 +0000

    Remove '/source' prefix for source get, delete

--- src/Controller/SourceRoutes.php
+++ src/Controller/SourceRoutes.php
@@ -8,5 +8,5 @@ class SourceRoutes
 {
     public const ATTRIBUTE_SOURCE_ID = 'sourceId';
     public const ROUTE_SOURCE_ID_PATTERN = '{' . self::ATTRIBUTE_SOURCE_ID . '<[A-Z90-9]{26}>}';
-    public const ROUTE_SOURCE = '/source/' . self::ROUTE_SOURCE_ID_PATTERN;
+    public const ROUTE_SOURCE = self::ROUTE_SOURCE_ID_PATTERN;
 }
