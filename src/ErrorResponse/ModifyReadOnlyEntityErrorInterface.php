028c786d50212e1d9fdf5415ee16c360b4a88ed1
Jon Cram <webignition@gmail.com>
2024-01-04 11:06:18 +0000

    EntityErrorInterface should be ModifyReadOnlyEntityErrorInterface

--- src/ErrorResponse/ModifyReadOnlyEntityErrorInterface.php
+++ src/ErrorResponse/ModifyReadOnlyEntityErrorInterface.php
@@ -8,15 +8,22 @@ use App\Entity\EntityIdentifierInterface;
 
 /**
  * @phpstan-type SerializedModifyReadOnlyEntityError array{
- *   class: non-empty-string,
+ *   class: 'modify_read_only',
  *   entity: array{
  *     id: non-empty-string,
  *     type: non-empty-string
  *   }
  * }
  */
-interface EntityErrorInterface extends ErrorInterface
+interface ModifyReadOnlyEntityErrorInterface extends ErrorInterface
 {
+    public const ERROR_CLASS = 'modify_read_only';
+
+    /**
+     * @return self::ERROR_CLASS
+     */
+    public function getClass(): string;
+
     public function getIdentifier(): EntityIdentifierInterface;
 
     /**
