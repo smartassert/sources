4a5e773ea78a2f93a1bc5d18834409a07585aa58
Jon Cram <webignition@gmail.com>
2024-02-01 11:44:33 +0000

    Use service-request 1.4

--- src/RequestParameter/Factory.php
+++ src/RequestParameter/Factory.php
@@ -4,27 +4,13 @@ declare(strict_types=1);
 
 namespace App\RequestParameter;
 
+use SmartAssert\ServiceRequest\Parameter\Factory as BaseFactory;
 use SmartAssert\ServiceRequest\Parameter\Parameter;
 use SmartAssert\ServiceRequest\Parameter\ParameterInterface;
 use SmartAssert\ServiceRequest\Parameter\Requirements;
-use SmartAssert\ServiceRequest\Parameter\Size;
 
-readonly class Factory
+readonly class Factory extends BaseFactory
 {
-    /**
-     * @param non-empty-string $name
-     */
-    public function createStringParameter(
-        string $name,
-        string $value,
-        int $minimumLength,
-        int $maximumLength
-    ): ParameterInterface {
-        return (new Parameter($name, $value))
-            ->withRequirements(new Requirements('string', new Size($minimumLength, $maximumLength)))
-        ;
-    }
-
     /**
      * @param non-empty-string $name
      */
