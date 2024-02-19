ca9b35a5fbc7591ade844124c5229f4aed8fab23
Jon Cram <webignition@gmail.com>
2024-02-19 16:08:40 +0000

    Resolve phpstan 'Asserted type * for * with type * does not narrow down the type'

--- src/Security/EntityAccessVoter.php
+++ src/Security/EntityAccessVoter.php
@@ -9,6 +9,9 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * @extends Voter<self::ACCESS, UserHeldEntityInterface>
+ */
 class EntityAccessVoter extends Voter
 {
     public const ACCESS = 'access';
