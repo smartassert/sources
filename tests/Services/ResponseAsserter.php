ee70ef8b83fd9f7576084384f1c983dd7cce665d
Jon Cram <webignition@gmail.com>
2024-10-16 15:11:41 +0100

    Remove ResponseAsserter::assertSerializeSuiteSuccessResponse()

--- tests/Services/ResponseAsserter.php
+++ tests/Services/ResponseAsserter.php
@@ -4,23 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests\Services;
 
-use App\Tests\Services\Asserter\Response\JsonResponseAsserter;
 use App\Tests\Services\Asserter\Response\YamlResponseAsserter;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class ResponseAsserter
 {
-    /**
-     * @param array<mixed> $expectedData
-     */
-    public function assertSerializeSuiteSuccessResponse(ResponseInterface $response, array $expectedData): void
-    {
-        (new JsonResponseAsserter(Response::HTTP_ACCEPTED, $expectedData))
-            ->assert($response)
-        ;
-    }
-
     public function assertReadSerializedSuiteSuccessResponse(ResponseInterface $response, string $expectedBody): void
     {
         (new YamlResponseAsserter(Response::HTTP_OK, $expectedBody))
