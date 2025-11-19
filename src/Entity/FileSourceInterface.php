2387c65458c7110715ec9057cb5eac6be222b159
Jon Cram <webignition@gmail.com>
2025-11-19 17:37:46 +0000

    Resolve FileSourceInterface controller arguments (#1507)

--- src/Entity/FileSourceInterface.php
+++ src/Entity/FileSourceInterface.php
@@ -6,10 +6,12 @@ namespace App\Entity;
 
 use App\Model\SourceRepositoryInterface;
 
-interface FileSourceInterface extends SourceInterface, SourceRepositoryInterface
+interface FileSourceInterface extends SourceInterface, SourceRepositoryInterface, IdentifiedEntityInterface
 {
     /**
      * @param non-empty-string $label
      */
     public function setLabel(string $label): static;
+
+    public function getIdentifier(): EntityIdentifierInterface;
 }
