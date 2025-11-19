9adaefa198099de09e7172970b4e040afc45a9fd
Jon Cram <webignition@gmail.com>
2025-11-19 18:06:04 +0000

    Use 001

--- src/Entity/SerializedSuiteInterface.php
+++ src/Entity/SerializedSuiteInterface.php
@@ -4,4 +4,15 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-interface SerializedSuiteInterface extends UserHeldEntityInterface, IdentifiedEntityInterface {}
+interface SerializedSuiteInterface extends UserHeldEntityInterface, IdentifiedEntityInterface
+{
+    /**
+     * @return non-empty-string
+     */
+    public function getId(): string;
+
+    /**
+     * @return array<string, string>
+     */
+    public function getParameters(): array;
+}
